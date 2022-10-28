da62b2859fe1197b9702c7cc9458c8eca4b36d2d
RP1minus1 <robin.pillar@1minus1.com>
2022-10-28 10:29:51 +0100

    adjusted styles

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -28,6 +28,10 @@ const resetInputs = () => {
 };
 
 const filteredComponents = computed(() => {
+  if (!searchValue.value && !categoryValue.value && !tagValue.value) {
+    return [];
+  }
+
   const search = searchValue.value.toLowerCase();
   const category = categoryValue.value.toLowerCase();
   const tag = tagValue.value.toLowerCase();
@@ -59,45 +63,41 @@ const filteredComponents = computed(() => {
 </script>
 
 <template>
-  <div
-    class="min-w-300 flex h-full flex-col overflow-y-scroll rounded-md bg-white/30 px-20 py-40 backdrop-blur-lg"
-  >
-    <div class="mb-20 border-b-2 border-gray-50 pb-20">
-      <client-only>
-        <FormInput
-          label="Search For Code"
-          name="search"
-          type="text"
-          suffix-icon="search"
-          v-model="searchValue"
-        />
-        <FormSelect
-          v-if="categories"
-          label="Categories"
-          type="select"
-          name="categories"
-          select-icon="down"
-          :options="[
-            'All',
-            ...categories.map((category) => category.attributes.name),
-          ]"
-          v-model="categoryValue"
-        />
-        <FormSelect
-          v-if="tags"
-          label="Tags"
-          type="select"
-          name="tags"
-          select-icon="down"
-          :options="['All', ...tags.map((tag) => tag.attributes.name)]"
-          v-model="tagValue"
-        />
-      </client-only>
+  <div class="relative flex h-full min-w-300 flex-col overflow-y-scroll py-40">
+    <div class="relative mb-20 px-20 pb-20">
+      <FormInput
+        label="Search For Code"
+        name="search"
+        type="text"
+        suffix-icon="search"
+        v-model="searchValue"
+      />
+      <FormSelect
+        v-if="categories"
+        label="Categories"
+        type="select"
+        name="categories"
+        select-icon="down"
+        :options="[
+          'All',
+          ...categories.map((category) => category.attributes.name),
+        ]"
+        v-model="categoryValue"
+      />
+      <FormSelect
+        v-if="tags"
+        label="Tags"
+        type="select"
+        name="tags"
+        select-icon="down"
+        :options="['All', ...tags.map((tag) => tag.attributes.name)]"
+        v-model="tagValue"
+      />
 
       <BtnBlock label="Reset" class="mt-15 w-full" @click="resetInputs" />
     </div>
 
-    <div class="flex flex-col space-y-20">
+    <div class="flex flex-col space-y-20 px-20">
       <BtnBlock
         v-for="(component, index) in filteredComponents"
         :key="index"
@@ -112,5 +112,7 @@ const filteredComponents = computed(() => {
         class="py-10 text-2xl text-gray-50 transition-colors duration-300 hover:text-gray-700"
       ></button> -->
     </div>
+
+    <div class="absolute right-0 top-50 bottom-50 w-[2px] bg-white"></div>
   </div>
 </template>
